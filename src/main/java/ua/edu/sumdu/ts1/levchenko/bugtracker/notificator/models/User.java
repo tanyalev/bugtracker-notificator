3f0551ba4b892895f9cc931be9664b5141bcb8d8
tanyalev <tanyalev.tom@gmail.com>
2020-03-15 22:26:10 +0200

    поправила модельку юзера

--- src/main/java/ua/edu/sumdu/ts1/levchenko/bugtracker/notificator/models/User.java
+++ src/main/java/ua/edu/sumdu/ts1/levchenko/bugtracker/notificator/models/User.java
@@ -3,29 +3,11 @@ package ua.edu.sumdu.ts1.levchenko.bugtracker.notificator.models;
 public class User {
     private String name;
     private String userId;
-    private String lastName;
-    private String loginName;
-    private String roleId;
-    private String password;
     private String email;
 
     public User(String userId, String name, String email) {
         this.name = name;
         this.userId = userId;
-    }
-
-    public User(String userId, String password, String roleId, String name) {
-        this.userId = userId;
-        this.password = password;
-        this.roleId = roleId;
-        this.name = name;
-    }
-
-    public User(String login, String password, String firstName, String lastName, String email) {
-        this.loginName = login;
-        this.password = password;
-        this.name = firstName;
-        this.lastName = lastName;
         this.email = email;
     }
 
@@ -45,38 +27,6 @@ public class User {
         this.userId = userId;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getLoginName() {
-        return loginName;
-    }
-
-    public void setLoginName(String loginName) {
-        this.loginName = loginName;
-    }
-
-    public String getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(String roleId) {
-        this.roleId = roleId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getEmail() {
         return email;
     }
@@ -90,10 +40,7 @@ public class User {
         return "User{" +
                 "name='" + name + '\'' +
                 ", userId='" + userId + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", loginName='" + loginName + '\'' +
-                ", roleId='" + roleId + '\'' +
-                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
                 '}';
     }
 }
